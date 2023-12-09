15b04a20687bb4b7d0b6e814f09ac8a4ec1a20ce
yailhamekasaputra <yailhameka11@gmail.com>
2023-12-09 21:05:54 +0700

    use speedinsights

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import About from './Pages/About/About';
 // import CustomPagination from './components/Pagination/CustomPagination';
 // import Container from '@mui/material/Container';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
       </div>
       <Footer />
       <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   );
 }
